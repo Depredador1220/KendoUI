989706c57e062cfab006bf0ada52bebc5ed1f7e4
Depredador1220 <62044516+Depredador1220@users.noreply.github.com>
2020-06-12 22:48:56 -0500

    Create 1.2.-personData.js

---
+++ 1.2.-personData.js
@@ -0,0 +1,36 @@
+var personData = [
+{
+	Nombre: "Andres",
+	Apellido: "Obrador",
+	Profesion: "Politico",
+	FDN: "11/08/1990",
+	ID: 1	
+},
+{
+	Nombre: "Mitchell",
+	Apellido: "Ginger",
+	Profesion: "Modelo",
+	FDN: "05/01/1974",
+	ID: 2 	
+},
+{
+	Nombre: "Orlando",
+	Apellido: "Esquivel",
+	Profesion: "Modelador 3D",
+	FDN: "23/06/2000",
+	ID: 3 	
+},
+{
+	Nombre: "Fabio",
+	Apellido: "Martinz",
+	Profesion: "Cocinero",
+	FDN: "28/12/1961",
+	ID: 4	
+},
+{
+	Nombre: "Christian",
+	Apellido: "Ruiz",
+	Profesion: "Desarrollador de Videojuegos",
+	FDN: "26/03/1989",
+	ID: 5	
+}];
